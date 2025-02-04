82241ea994a22eb3ae9b29e5228f1960f41439c5
seifmigo <seifmigo1234@gmail.com>
2025-02-04 21:42:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,8 @@ function showMessage(response) {
 
     // Update text content, show message, and change image source to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "LESGOOO see you on the 14th babygirl😘😘";
+    yesMessage.textContent = "bahseb eftkrtik hadosy no 😘😘
+      yala ashoofik ba2a yom 14 aw 15";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
     document.getElementsByClassName("image")[0].src = "images/dance.gif";
